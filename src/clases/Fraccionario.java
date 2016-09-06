1bd65d99e98379d15afaeeff782dae58c04dd241
“LenninLara” <lenninlara28@hotmail.com>
2016-09-06 11:28:04 -0500

    Interfaz Terminada

--- src/clases/Fraccionario.java
+++ src/clases/Fraccionario.java
@@ -11,5 +11,37 @@ package clases;
  * @author llara11
  */
 public class Fraccionario {
+    private int numerador;
+    private int denominador;
+    
+    public Fraccionario (int numerador, int denominador){
+        this.numerador=numerador;
+        this.denominador=denominador;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+    
+    public Fraccionario Sumar(Fraccionario f2){
+        Fraccionario f;
+        int num,den;
+        num=this.numerador * f2.denominador + this.denominador * f2.numerador;
+        den=this.denominador * f2.denominador;
+        f = new Fraccionario(num,den);
+        return f;
+    }
     
 }
